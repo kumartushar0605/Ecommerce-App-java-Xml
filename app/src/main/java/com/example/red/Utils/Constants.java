b311c77eb8931caa5fd2ecb2b026026dd4c3f5f2
Tushar Kumar <108176375+kumartushar0605@users.noreply.github.com>
2024-07-11 01:22:04 +0530

    Update Constants.java

--- app/src/main/java/com/example/red/Utils/Constants.java
+++ app/src/main/java/com/example/red/Utils/Constants.java
@@ -1,7 +1,7 @@
 package com.example.red.Utils;
 
 public class Constants {
-    public static String API_BASE_URL = "https://tutorials.mianasad.com/ecommerce";
+    public static String API_BASE_URL = "";
     public static String GET_CATEGORIES_URL = API_BASE_URL + "/services/listCategory";
     public static String GET_PRODUCTS_URL = API_BASE_URL + "/services/listProduct";
     public static String GET_OFFERS_URL = API_BASE_URL + "/services/listFeaturedNews";
